a0276ff2542a53e20d62a29fd5d457c148a82aac
Alexander <38163966+KislyakovS@users.noreply.github.com>
2021-05-12 22:40:10 +0300

    Routes file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce/constants.dart';
+import 'package:e_commerce/routs.dart';
 import 'package:e_commerce/screens/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -19,7 +20,8 @@ class MyApp extends StatelessWidget {
             body1: TextStyle(color: kTextColor),
             body2: TextStyle(color: kTextColor)),
       ),
-      home: SplashScreen(),
+      initialRoute: SplashScreen.routeName,
+      routes: routes,
     );
   }
 }
--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -3,6 +3,8 @@ import 'package:e_commerce/size_config.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
+  static String routeName = '/spllash';
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
