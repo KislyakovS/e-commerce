09051910928c3fb3c82300a6e78455b1fdb19897
Alexander <38163966+KislyakovS@users.noreply.github.com>
2021-05-12 21:24:36 +0300

    Init base project

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:e_commerce/constants.dart';
+import 'package:e_commerce/screens/splash/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Muli',
+        textTheme: TextTheme(
+            body1: TextStyle(color: kTextColor),
+            body2: TextStyle(color: kTextColor)),
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
