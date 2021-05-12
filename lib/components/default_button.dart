7857e6b065b5357c86b99673fd7c4bfccf0db2d7
Alexander <38163966+KislyakovS@users.noreply.github.com>
2021-05-12 22:35:24 +0300

    Added default button

---
+++ lib/components/default_button.dart
@@ -0,0 +1,42 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+import '../size_config.dart';
+
+class DefaultButton extends StatelessWidget {
+  const DefaultButton({
+    Key key,
+    @required this.text,
+    @required this.press,
+  }) : super(key: key);
+  final String text;
+  final void Function() press;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: getProportionateScreenHeight(56),
+      child: TextButton(
+        onPressed: press,
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+          backgroundColor: MaterialStateProperty.all<Color>(kPrimaryColor),
+        ),
+        child: Text(
+          'Continue',
+          style: TextStyle(
+            fontSize: getProportionateScreenWidth(18),
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/splash/components/body.dart
+++ lib/screens/splash/components/body.dart
@@ -1,6 +1,9 @@
+import 'package:e_commerce/components/default_button.dart';
+import 'package:e_commerce/screens/splash/components/splash_content.dart';
+import 'package:e_commerce/size_config.dart';
 import 'package:flutter/material.dart';
 
-import 'splash_content.dart';
+import '../../../constants.dart';
 
 class Body extends StatefulWidget {
   @override
@@ -8,6 +11,7 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  int currentPage = 0;
   List<Map<String, String>> splashData = [
     {
       'image': 'assets/images/splash_1.png',
@@ -34,16 +38,58 @@ class _BodyState extends State<Body> {
             Expanded(
               flex: 3,
               child: PageView.builder(
-                itemCount: splashData.length,
-                itemBuilder: (context, index) => SplashContent(
-                  image: splashData[index]['image'],
-                  text: splashData[index]['text'],
+                  itemCount: splashData.length,
+                  itemBuilder: (context, index) => SplashContent(
+                        image: splashData[index]['image'],
+                        text: splashData[index]['text'],
+                      ),
+                  onPageChanged: (index) {
+                    setState(() {
+                      currentPage = index;
+                    });
+                  }),
+            ),
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: getProportionateScreenWidth(20),
+                ),
+                child: Column(
+                  children: [
+                    const Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(
+                        splashData.length,
+                        (index) => buildDot(index: index),
+                      ),
+                    ),
+                    const Spacer(flex: 2),
+                    const DefaultButton(
+                      text: 'Continue',
+                      press: () {},
+                    ),
+                    const Spacer(),
+                  ],
                 ),
               ),
             ),
-            Expanded(flex: 2, child: SizedBox())
           ],
         ),
+      ),
+    );
+  }
+
+  AnimatedContainer buildDot({int index}) {
+    return AnimatedContainer(
+      duration: kAnimationDuration,
+      margin: const EdgeInsets.only(right: 5),
+      width: index == currentPage ? 20 : 6,
+      height: 6,
+      decoration: BoxDecoration(
+        color: index == currentPage ? kPrimaryColor : const Color(0xFFD8D8D8),
+        borderRadius: BorderRadius.circular(3),
       ),
     );
   }
