82c112c2728f3e7d217910fb4c4c7d7150b9acfa
Alexander <38163966+KislyakovS@users.noreply.github.com>
2021-05-13 14:13:45 +0300

    Update theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -5,17 +5,43 @@ import 'constants.dart';
 ThemeData theme() {
   return ThemeData(
     scaffoldBackgroundColor: Colors.white,
-    appBarTheme: const AppBarTheme(
-      backgroundColor: Colors.white,
-      elevation: 0,
-      brightness: Brightness.light,
-      iconTheme: IconThemeData(color: Colors.black),
-      textTheme: TextTheme(
-          headline6: TextStyle(color: Color(0xFF8B8B8B), fontSize: 18)),
-    ),
+    appBarTheme: appBarTheme(),
+    inputDecorationTheme: inputDecorationTheme(),
     fontFamily: 'Muli',
-    textTheme: const TextTheme(
-        body1: TextStyle(color: kTextColor),
-        body2: TextStyle(color: kTextColor)),
+    textTheme: textTheme(),
+  );
+}
+
+AppBarTheme appBarTheme() {
+  return const AppBarTheme(
+    backgroundColor: Colors.white,
+    elevation: 0,
+    brightness: Brightness.light,
+    iconTheme: IconThemeData(color: Colors.black),
+    textTheme:
+        TextTheme(headline6: TextStyle(color: Color(0xFF8B8B8B), fontSize: 18)),
+  );
+}
+
+TextTheme textTheme() {
+  return const TextTheme(
+    body1: TextStyle(color: kTextColor),
+    body2: TextStyle(color: kTextColor),
+  );
+}
+
+InputDecorationTheme inputDecorationTheme() {
+  return InputDecorationTheme(
+    contentPadding: const EdgeInsets.symmetric(horizontal: 45, vertical: 20),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(28),
+      borderSide: const BorderSide(color: kTextColor),
+      gapPadding: 10,
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(28),
+      borderSide: const BorderSide(color: kTextColor),
+      gapPadding: 10,
+    ),
   );
 }
