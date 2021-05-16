3d3c7b9f2bb79227177c5315d2ef0ae09452a965
Alexander <38163966+KislyakovS@users.noreply.github.com>
2021-05-16 10:33:41 +0300

    Added icon button

--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -1,4 +1,6 @@
+import 'package:e_commerce/components/rounded_con_button.dart';
 import 'package:e_commerce/models/Product.dart';
+import 'package:e_commerce/size_config.dart';
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
@@ -7,7 +9,14 @@ class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      backgroundColor: Color(0xFFF5F6F9),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: RoundedIconButton(
+          iconData: Icons.arrow_back_ios,
+          press: () => Navigator.pop(context),
+        ),
+      ),
     );
   }
 }
